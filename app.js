d6721ddf8d746084a8394548b393689300bd9b79
MarcoAFalcon <marco.a.falcon@outlook.com>
2025-03-17 20:45:07 -0300

    funcion agregar amigos agregada

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == '') {
+        alert("Por favor, inserte un nombre.")
+    }else{
+        amigos.push(amigo);
+        limpiar();
+    }
+    console.log(amigos);
+}
+
+
+
+function limpiar() {
+    document.getElementById('amigo').value = '';
+}
