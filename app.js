178809cb22d916166e4de132b664b30dc4572e41
MarcoAFalcon <marco.a.falcon@outlook.com>
2025-03-18 02:57:05 -0300

    funcion amigo sorteado agregada

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = ['']
+let amigos = []
 
 //toma el valor que haya en el ID amigo, valida que el input tenga un valor y si lo hay lo guarda en el array amigos
 function agregarAmigo() {
@@ -13,7 +13,8 @@ function agregarAmigo() {
         mostrarAmigos()
     }
 }
-//iteracion para agregar en la lista con ID listaAmigos los nombres en la lista amigos
+
+//iteracion para agregar en la lista con ID listaAmigos los nombres en el array amigos
 function mostrarAmigos() {
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = '';         
@@ -22,6 +23,20 @@ function mostrarAmigos() {
         }
 }
 
+//funcion para sortear amigos
+function sortearAmigo() {
+    let resultado = document.getElementById('resultado');
+    let indiceDeLista = Math.floor(Math.random()*amigos.length);
+    if (amigos == '' ) {
+        alert("Por favor, inserte mas de un nombre.");
+    } else {
+        resultado.innerHTML = `El amigo secreto es...${amigos[indiceDeLista]}`
+    }
+}
+
+//FUNCIONES COMPLEMENTARIAS
+
+//limpiar espacio obtenido por id
 function limpiar(id,valor) {
     document.getElementById(id).value =valor ;
 }
