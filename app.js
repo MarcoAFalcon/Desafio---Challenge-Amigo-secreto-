e9da70bbf691f60ee8441d099c445ceec6bc572b
MarcoAFalcon <marco.a.falcon@outlook.com>
2025-03-18 02:07:52 -0300

    funcion de lista de amigos agregada

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = []
+let amigos = ['']
 
+//toma el valor que haya en el ID amigo, valida que el input tenga un valor y si lo hay lo guarda en el array amigos
 function agregarAmigo() {
     let amigo = document.getElementById('amigo').value;
     if (amigo == '') {
-        alert("Por favor, inserte un nombre.")
+        alert("Por favor, inserte un nombre.");
     }else{
         amigos.push(amigo);
-        limpiar();
+        limpiar('amigo','');
+        mostrarAmigos()
     }
-    console.log(amigos);
+}
+//iteracion para agregar en la lista con ID listaAmigos los nombres en la lista amigos
+function mostrarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';         
+        for (let index = 0; index < amigos.length; index++) {
+            listaAmigos.innerHTML += `<li>${amigos[index]}</li>`;
+        }
 }
 
-
-
-function limpiar() {
-    document.getElementById('amigo').value = '';
+function limpiar(id,valor) {
+    document.getElementById(id).value =valor ;
 }
